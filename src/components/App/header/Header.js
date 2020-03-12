26c9b734a742871a5f4698ec6bbb8f50351b782b
mlyaf <mlyaf@adux.com>
2020-03-12 17:25:09 +0100

    setting up header width adding its components

---
+++ src/components/App/header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './header.scss';
+
+import HomeButton from "../../UI/homeButton/HomeButton";
+import TitleHeader from "../../UI/titleHeader/TitleHeader"
+import WhiteButton from "../../UI/buttons/WhiteButton"
+import BlueButton from "../../UI/buttons/BlueButton"
+
+function Header() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <HomeButton/>
+                <TitleHeader/>
+                <div>
+                    <WhiteButton content={'Inscription'} />
+                    <BlueButton content={'Connexion'} />
+                </div>
+
+            </header>
+        </div>
+    );
+}
+
+export default Header;
--- src/components/UI/titleHeader/TitleHeader.js
+++ src/components/UI/titleHeader/TitleHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './titleHeader.scss'
 import Icon from "../../../images/titleHeader.svg";
 
 function titleHeader() {
   return (
     <>
-      <img src={Icon}/>
+      <img className='titleHeader' src={Icon}/>
     </>
   );
 }
