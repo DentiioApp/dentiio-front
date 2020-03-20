e54d8760a153990a73b527dd702af0b2cc190245
Romain <maucotel.romain@gmail.com>
2020-03-20 19:37:28 +0100

    Landing Page

--- src/components/App/header/Header.js
+++ src/components/App/header/Header.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import './header.scss'
-
-import HomeButton from '../../UI/homeButton/HomeButton'
 import TitleHeader from '../../UI/titleHeader/TitleHeader'
-import WhiteButton from '../../UI/buttons/WhiteButton'
-import BlueButton from '../../UI/buttons/BlueButton'
 
 function Header () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <HomeButton />
+        {/*<HomeButton />*/}
         <TitleHeader />
-        <div>
+        {/*<div>
           <WhiteButton content='Inscription' />
           <BlueButton content='Connexion' />
-        </div>
+        </div>*/}
       </header>
     </div>
   )
