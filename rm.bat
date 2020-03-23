680404912ee5cc7b1721e65602963c7c661942cf
Lory LETICEE <loryleticee@gmail.com>
2020-03-23 23:13:50 +0100

    add logo ok

---
+++ rm.bat
@@ -0,0 +1,13 @@
+rm -rf .idea favicon.ico static index.html manifest.json logo192.png logo512.png build asset-manifest.json service-worker.js precache-manifest.*.js node_modules &&
+yarn
+#mv ../.env . &&
+#yarn build &&
+#mv .env ../ &&
+#cd build &&
+#mv ./* ../ &&
+#rm -rf build
+echo FINISH
+
+
+
+
