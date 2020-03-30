c7e2310fa697e637c266b84396b899fb877bb790
Lory LETICEE <loryleticee@gmail.com>
2020-03-30 04:33:17 +0200

    add redux redux-thunk react-dom , route etc ... route and redux install and active OK

---
+++ src/store/reducers/log.js
@@ -0,0 +1,12 @@
+import {LOG_USER} from '../actions'
+
+const INIT_STATE = "";
+
+export const userName = (state = INIT_STATE, action) => {
+    switch (action.type) {
+        case LOG_USER :
+            return state = action.username;
+        default :
+            return state;
+    }
+};
