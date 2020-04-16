e57155f83aa903d07b6854b3deef696834a12512
mlyaf <mlyaf@adux.com>
2020-04-16 19:04:57 +0200

    register css v1

--- src/components/App/Register/register.js
+++ src/components/App/Register/register.js
@@ -1,282 +1,264 @@
-import './register.scss'
-
-import React, { useState } from 'react'
-import { useHistory, Redirect } from 'react-router-dom'
-
-import MenuItem from '@material-ui/core/MenuItem'
-import { makeStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff'
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton'
-import InputLabel from '@material-ui/core/InputLabel'
-import InputAdornment from '@material-ui/core/InputAdornment'
-import Button from '@material-ui/core/Button'
-import FormHelperText from '@material-ui/core/FormHelperText'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { registerUser } from '../../../store/actions'
-import img from '../../../images/auth.svg'
-import Grid from '@material-ui/core/Grid';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import ResponsiveCard from "../../ResponsiveDesign/responsiveCard";
-import ResponsiveContainerGrid from "../../ResponsiveDesign/responsiveContainerGrid";
+import {
+  Avatar,
+  CssBaseline,
+  FormControlLabel,
+  Paper,
+  Typography,
+} from "@material-ui/core/";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useHistory } from "react-router-dom";
+import img from "../../../images/auth.svg";
+import { registerUser } from "../../../store/actions";
+import GradientBtn from "../../UI/buttons/GradientBtn";
+import "./register.scss";
 
 // API DATAS
 const currencies = [
   {
-    value: 'CD',
-    label: 'Chirurgien Dentiste'
+    value: "CD",
+    label: "Chirurgien Dentiste",
   },
   {
-    value: 'DI',
-    label: 'Dentiste Interne'
-  }
-]
+    value: "DI",
+    label: "Dentiste Interne",
+  },
+];
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1.5),
-      width: 250
-    }
-  }
-}))
+    height: "85vh",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "80%",
+  },
+  image: {
+    backgroundImage: `url(${img})`,
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  GradientBtn: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const SignUp = () => {
-  const history = useHistory()
-  const classes = useStyles()
-  const dispatch = useDispatch()
+  const history = useHistory();
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const user = useSelector(state => state.user)
+  const user = useSelector((state) => state.user);
 
   const initValues = {
-    pseudo: '',
-    email: '',
-    password: '',
-    function: '',
+    pseudo: "",
+    email: "",
+    password: "",
+    function: "",
     showPassword: false,
     cgu: false,
-  }
+  };
 
-  const [values, setValues] = useState(initValues)
-  const [errEmail, setErrEmail] = useState('Dupont@dupont.fr')
-  const [errPassword, setErrPassword] = useState(false)
-  const [errPseudo, setErrPseudo] = useState(false)
-  const [errCgu, setErrCgu ]= useState(false)
+  const [values, setValues] = useState(initValues);
+  const [errEmail, setErrEmail] = useState("Dupont@dupont.fr");
+  const [errPassword, setErrPassword] = useState(false);
+  const [errPseudo, setErrPseudo] = useState(false);
+  const [errCgu, setErrCgu] = useState(false);
 
   const catchSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (values.password === '' && values.function === '' && values.pseudo === '') { return false }
-    if (checkEmail(values.email) === false) { setErrEmail(true) ; return false; }
-    if (checkPassword(values.password) === false) { setErrPassword(true) ; return false }
-    if (existEmail(values.email) === true) { setErrEmail(true) ; return false }
-    if (values.cgu === false) {setErrCgu(true) ; return false }
+    if (
+      values.password === "" &&
+      values.function === "" &&
+      values.pseudo === ""
+    ) {
+      return false;
+    }
+    if (checkEmail(values.email) === false) {
+      setErrEmail(true);
+      return false;
+    }
+    if (checkPassword(values.password) === false) {
+      setErrPassword(true);
+      return false;
+    }
+    if (existEmail(values.email) === true) {
+      setErrEmail(true);
+      return false;
+    }
+    if (values.cgu === false) {
+      setErrCgu(true);
+      return false;
+    }
 
-    dispatch(registerUser({
-      pseudo: values.pseudo,
-      email: values.email,
-      password: values.password,
-      function: values.function,
-      cgu: values.cgu,
-    }))
-  }
-
+    dispatch(
+      registerUser({
+        pseudo: values.pseudo,
+        email: values.email,
+        password: values.password,
+        function: values.function,
+        cgu: values.cgu,
+      })
+    );
+  };
 
   // Check Valid email
   const checkEmail = (email) => {
-    return (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email))
-  }
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  };
 
   // Exist  email
   const existEmail = (email) => {
-    const emails = ['loryleticee@gmail.com', 'lory@lory.com', 'lo@lo.fr']
-    return emails.includes(email)
-  }
+    const emails = ["loryleticee@gmail.com", "lory@lory.com", "lo@lo.fr"];
+    return emails.includes(email);
+  };
 
   // Check Valid password
   const checkPassword = (password) => {
     // speial chars , upper letter , lower letter, number more than 7 chars
-    return (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#'<>"#?¨áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ()),$%^+=\-_°\\:/&.;|*])(?=.{8,})/.test(password))
-  }
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#'<>"#?¨áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ()),$%^+=\-_°\\:/&.;|*])(?=.{8,})/.test(
+      password
+    );
+  };
 
-  const handleChange = prop => event => {
-    if (prop === 'pseudo') {
-      if (event.target.value === false || event.target.value === '') {
-        setErrPseudo(true)
-      }else{
-        setErrPseudo(false)
+  const handleChange = (prop) => (event) => {
+    if (prop === "pseudo") {
+      if (event.target.value === false || event.target.value === "") {
+        setErrPseudo(true);
+      } else {
+        setErrPseudo(false);
       }
     }
-    if (prop === 'email') {
-      if (checkEmail(event.target.value) === false || existEmail(event.target.value) === true) {
-        setErrEmail(event.target.value)
+    if (prop === "email") {
+      if (
+        checkEmail(event.target.value) === false ||
+        existEmail(event.target.value) === true
+      ) {
+        setErrEmail(event.target.value);
       }
     }
-    if (prop === 'password') {
+    if (prop === "password") {
       if (checkPassword(event.target.value) === false) {
-        setErrPassword(event.target.value)
+        setErrPassword(event.target.value);
       }
     }
-    if (prop === 'cgu') {
+    if (prop === "cgu") {
       if (event.target.value === false) {
-        setErrCgu(true)
-      } else{
-        setErrCgu(false)
+        setErrCgu(true);
+      } else {
+        setErrCgu(false);
       }
     }
-    
-    setValues({ ...values, [prop]: event.target.value })
-  }
+
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
-  }
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
   const handleClickCgu = () => {
-    setValues({ ...values, cgu: !values.cgu })
-  }
-  const handleMouseDownCgu = event => {
-    event.preventDefault()
-  }
-  const handleMouseDownPassword = event => {
-    event.preventDefault()
-  }
+    setValues({ ...values, cgu: !values.cgu });
+  };
+  const handleMouseDownCgu = (event) => {
+    event.preventDefault();
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   if (user.details !== undefined) {
     if (user.connected === false) {
-      history.push('/', { content: 'connexion' })
+      history.push("/", { content: "connexion" });
     } else {
-      return <Redirect to='/account' />
+      return <Redirect to="/account" />;
     }
-  };
+  }
 
   return (
-    <>
-      <div className='register'>
-        <ResponsiveContainerGrid>
-          <Grid container >
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}></Avatar>
+          <Typography component="h1" variant="h5">
+            Inscription
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Pseudo"
+              type="text"
+              id="pseudo"
+              autoComplete="current-password"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <br></br> <br></br> <br></br>
+            <GradientBtn
+              variant="contained"
+              type="submit"
+              className="GradientBtn"
+            />
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
 
-
-            <Grid item xs={0} sm={6} lg={6} xl={6}>
-              <div className='home-bg'>
-                <img src={img} alt=""/>
-              </div>
-            </Grid>
-            <Grid item xs={12} sm={6} lg={6} xl={6} className='form'>
-              <div className='form'>
-                <ResponsiveCard>
-
-
-
-                  <form className={classes.root} noValidate autoComplete='off'>
-                    <CardHeader title="Sign in" subheader="to continue to kTPWC" />
-                    <CardContent>
-                      <InputLabel className='inputLabel'>
-                        Pseudo* :
-                      </InputLabel>
-                      <TextField
-                          className='textField'
-                          id='pseudo-basic' required
-                          value={values.pseudo}
-                          onChange={handleChange('pseudo')}
-                          variant={'outlined'}
-                          error={errPseudo}
-                      />
-                      <br/>
-
-                      <InputLabel className='inputLabel'>
-                        Email* :
-                      </InputLabel>
-                      <TextField
-                        className='textField'
-                        id='email-basic' required
-                        value={values.email}
-                        error={errEmail === ""}
-                        onChange={handleChange('email')}
-                        placeholder='Dupont@dupont.fr'
-                        variant={'outlined'}
-                        helperText={values.email !== '' ? (checkEmail(values.email) === false ? 'Email invalide!' : ' ') :''}
-                      />
-                      <FormHelperText id='my-helper-text'>On ne partagera jamais votre email.</FormHelperText>
-                      <br/>
-
-                      <InputLabel className='inputLabel'>
-                        Vous êtes* :
-                      </InputLabel>
-                      <TextField
-                        className='textField'
-                        id='filled-select-currency'
-                        select
-                        value={values.currency}
-                        onChange={handleChange}
-                        helperText='Selectionnez votre fonction'
-                        variant='outlined'
-                      >
-                        {currencies.map(option => (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                          </MenuItem>
-                        ))}
-                      </TextField>
-
-                      <InputLabel className='inputLabel'>
-                        Mot de passe* :
-                      </InputLabel>
-                      <TextField
-                          className='textField'
-                          id='outlined-adornment-password'
-                          type={values.showPassword ? 'text' : 'password'}
-                          value={values.password}
-                          variant='outlined'
-                          error={errPassword === ""}
-                          onChange={handleChange('password')}
-                          autoComplete="on"
-                          endAdornment={
-                            <InputAdornment position='start'>
-                              <IconButton
-                                  aria-label='toggle password visibility'
-                                  onClick={handleClickShowPassword}
-                                  onMouseDown={handleMouseDownPassword}
-                                  edge='end'
-                              >
-                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                              </IconButton>
-                            </InputAdornment>
-                          }
-                          labelWidth={20}
-                      />
-                    </CardContent>
-
-                    <Button
-                      variant="contained"
-                      size="large"
-                      color="primary"
-                      onClick={catchSubmit}
-                      name='submit_button'
-                    >
-                      S'inscrire
-                    </Button>
-
-                    <Checkbox 
-                      required
-                      checked ={values.cgu}
-                      onClick={handleClickCgu}
-                      onMouseDown={handleMouseDownCgu}
-                      error={errCgu}
-                    />
-                  </form>
-
-                </ResponsiveCard>
-              </div>
-           </Grid>
-          </Grid>
-        </ResponsiveContainerGrid>
-      </div>
-    </>
-  )
-}
-
-export default SignUp
+export default SignUp;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,55 +1,52 @@
-import './Home.scss'
-
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import { setup } from '../../services/LoginCheck'
-import Header from '../../components/App/Header/header'
-import Register from '../../components/App/Register/register'
-import SignIn from '../../components/App/SignIn/signIn'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import Register from "../../components/App/Register/register";
+import SignIn from "../../components/App/SignIn/signIn";
+import { setup } from "../../services/LoginCheck";
+import "./Home.scss";
 
 const Home = (props) => {
-  const user = useSelector(state => state.user)
+  const user = useSelector((state) => state.user);
 
-  var auth
-  var action = ''
+  var auth;
+  var action = "";
   //
   var isSetup = setup();
-  console.log('isSetup :', isSetup)
+  console.log("isSetup :", isSetup);
 
   if (props.location.state === undefined) {
-    action = 'inscription'
+    action = "inscription";
   } else {
-    if (props.location.state.content === 'connexion') {
-      action = 'connexion'
+    if (props.location.state.content === "connexion") {
+      action = "connexion";
     } else {
-      action = 'inscription'
+      action = "inscription";
     }
   }
 
-  if (action === 'inscription') {
-    auth = <Register />
+  if (action === "inscription") {
+    auth = <Register />;
   }
 
-  if (action === 'connexion') {
-    auth = <SignIn />
+  if (action === "connexion") {
+    auth = <SignIn />;
   }
 
   if (user.details !== undefined) {
     if (user.connected === false) {
-      auth = <SignIn />
+      auth = <SignIn />;
     } else {
-      return <Redirect to='/account' />
+      return <Redirect to="/account" />;
     }
-  };
+  }
 
   return (
-    <div className='App'>
-      <Header content={action} />
+    <div className="App">
+      {/* <Header content={action} /> */}
       {auth}
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
