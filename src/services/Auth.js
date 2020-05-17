ab4c03cd6d4f31eab236da693adce46c1df34822
Lory LETICEE <loryleticee@gmail.com>
2020-05-17 04:55:31 +0200

    add props to Gradient button , change name and process for user subscriibbng

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -15,20 +15,26 @@ export const logout = () => {
 }
 
 // Verifie si un token existe dans le localStorage
-export const setup = () => {
+export const setup = (subscribe = undefined) => {
   const token = window.localStorage.getItem('authToken')
+  const isSubscribing = subscribe
   var connect = false
 
-  if (token) {
-    const jwtData = jwtDecode(token)
-    if (jwtData.exp * 1000 > new Date().getTime()) {
-      connect = true
+  if(isSubscribing) {
+    //
+  } else {
+    if (token) {
+      window.localStorage.removeItem('authSubscribeMsg')
+      const jwtData = jwtDecode(token)
+      if (jwtData.exp * 1000 > new Date().getTime()) {
+        connect = true
+      } else {
+        logout()
+      }
     } else {
       logout()
     }
-  } else {
-    logout()
-  }
 
-  return connect
+    return connect
+  }
 }
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+import { checkPseudo } from './Auth/checkPseudo'
+import { checkEmail } from './Auth/checkEmail'
+import { checkPassword } from './Auth/checkPassword'
+
+export { checkPseudo }
+export { checkEmail }
+export { checkPassword }
+
