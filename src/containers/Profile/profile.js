820f0e76d0bd29fefc343c0262000b688649a442
mlyaf <mlyaf@adux.com>
2020-06-26 14:38:23 +0200

    add logout button

--- src/containers/Profile/profile.js
+++ src/containers/Profile/profile.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Header from '../../components/App/Header/header'
+import GradientBtn from "../../components/UI/buttons/GradientBtn";
+import {makeStyles} from "@material-ui/core/styles";
+import oStyle from "../../components/App/Register/registerStyle";
+
+
+function logout(){
+    localStorage.clear();
+    window.location.href = '/';
+}
+
+
 
 const Profile = () => {
+
+
   return (
     <>
-      <Header target='profile' />
+        <Header target='profile' />
+        <button onClick={logout}>
+            Se déconnecter
+        </button>
+
     </>
   )
 }
