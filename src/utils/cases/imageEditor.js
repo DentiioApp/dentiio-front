288727c57ced2445aa80c0288b11c7546931f5b3
Lory LÉTICÉE <loryleticee@gmail.com>
2021-01-28 22:21:09 +0100

    Add packages merge-images

---
+++ src/utils/cases/imageEditor.js
@@ -0,0 +1,19 @@
+import mergeImages from 'merge-images';
+import censoring_img from '.svg'
+
+var CENSORED_IMG = [{ src: censoring_img, x: 0, y: 0 }];
+let array_to_merge = [];
+export const imageEditor = async (images, aCoordonne = CENSORED_IMG) => {
+  /** PERMET DE COLLER DES IMAGES LES UNE SUR LES AUTRES A DES COORDONNÉE ET GÉNÉREER LE B64  */
+  aCoordonne.forEach(element => {
+    array_to_merge.push()
+  });
+  
+  await mergeImages([
+    { src: censoring_img, x: 0, y: 0 },
+    { src: 'http://localhost:3000/logo192.png', x: 32, y: 0 },
+    { src: 'http://localhost:3000/logo512.png', x: 16, y: 0 }
+  ])
+    .then((b64) => b64)
+
+}
