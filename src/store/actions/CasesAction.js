70f7cafcef2dc6711088a10cd7454a8add7db403
Lory LÉTICÉE <loryleticee@gmail.com>
2021-02-09 20:49:26 +0100

    asynchrone response pst image starting

--- src/store/actions/CasesAction.js
+++ src/store/actions/CasesAction.js
@@ -73,15 +73,21 @@ export const post_images = async (files, id_clinical_omni, type) => {
   let incre_index_img = 0;
   let stop = false;
   let IS_PRINCIPAL = false;
+  let errorSend = false
 
   let intervalID = setInterval(() => {
     if (incre_index_img < files.length) {
       IS_PRINCIPAL = incre_index_img === 0 ? true : false;
 
-      insertImage(files[incre_index_img], id_clinical_omni, IS_PRINCIPAL, type)
+      insertImage(files[incre_index_img], id_clinical_omni, IS_PRINCIPAL, type).then((res)=>{
+        if(res.datas.id == undefined) {
+          errorSend = true;
+        }
+      })
       incre_index_img += 1;
     } else {
       stop = true;
+      localStorage.setItem('finishloadimg','true')
     }
 
     if (stop) clearInterval(intervalID);
